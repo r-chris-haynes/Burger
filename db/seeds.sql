e3415212a99244ccc1cc6847cd705639a0d2a465
chris haynes <chrishaynes@chriss-mbp.home>
2019-06-19 11:59:55 -0400

    server.js created and db directory with mysql schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Swiss Burger", "True");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chili Slaw Burger", "True");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheddar Burger", "True");
