889de6118647606465d41390c28007878933a672
chris haynes <chrishaynes@chriss-mbp.home>
2019-06-20 15:19:49 -0400

    working on  setting up routing

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Swiss Burger", "True");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Chili Slaw Burger", "True");
-INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheddar Burger", "True");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Swiss Burger", "False");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chili Slaw Burger", "False");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheddar Burger", "False");
