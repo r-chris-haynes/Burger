31d359f59afed65fe7f07b3f36c9d3fa78b7d5be
chris haynes <r.chris.haynes@gmail.com>
2019-09-04 17:29:45 -0400

    added delete buttons

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -43,6 +43,21 @@ router.get("/", function(req, res) {
     });
   });
   
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.delete( condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.json(result);
+      }
+    });
+  });
+  
  
 
 module.exports = router;
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -38,5 +38,23 @@ $(function () {
         );
     });
 
+    $(document).on("click", ".del-burger", function (event) {
+        event.preventDefault();
+
+        var id = $(this).attr("data-id");
+        console.log("id: ",id);
+        
+        // Send the eat request.
+        $.ajax("/api/burgers/" + id, {
+            type: "DELETE",
+        }).then(
+            function () {
+                console.log("devoured burger", id);
+               
+                location.reload();
+            }
+        );
+    });
+
    
 });
